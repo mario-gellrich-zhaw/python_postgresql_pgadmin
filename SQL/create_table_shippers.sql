7e0b9dcccfae7494e85a57b2c605e735c54815c3
Mario Gellrich <mario.gellrich@gmx.ch>
2024-09-09 19:18:18 +0200

    ...

---
+++ SQL/create_table_shippers.sql
@@ -0,0 +1,9 @@
+CREATE TABLE tableName (
+    ShipperID	VARCHAR(512),
+    ShipperName	VARCHAR(512),
+    Phone	VARCHAR(512)
+);
+
+INSERT INTO tableName (ShipperID, ShipperName, Phone) VALUES ('1', 'Speedy Express', '(503) 555-9831');
+INSERT INTO tableName (ShipperID, ShipperName, Phone) VALUES ('2', 'United Package', '(503) 555-3199');
+INSERT INTO tableName (ShipperID, ShipperName, Phone) VALUES ('3', 'Federal Shipping', '(503) 555-9931');
