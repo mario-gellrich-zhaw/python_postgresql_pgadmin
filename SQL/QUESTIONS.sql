348e5ddbb6285910ad1e6dd9f54177bc32f3d009
Mario Gellrich <mario.gellrich@gmx.ch>
2024-09-15 14:06:14 +0200

    ...

---
+++ SQL/QUESTIONS.sql
@@ -0,0 +1,32 @@
+
+1. What are the details of all customers whose country is 'Spain'?
+
+2. What are the distinct cities of customers from Germany with a city containing the letter 'B'?
+
+3. What are the number of orders placed by each customer? Sort the result by the number of orders in descending order.
+
+4. What are the customers who have placed more than 3 orders?
+
+5. What are the top 5 most expensive products? Round the price to 2 decimal places.
+
+6. What are the order details (ProductID, Quantity) for customers from France?
+
+7. Area there products without a category assigned?
+
+8. What are all orders and their employees?
+
+9. What is the average, minimum, and maximum price of products? Round the values to 2 decimal places.
+
+10. What are the products with prices between 10 and 50? Round the price to 2 decimal places and sort the result by price in descending order.
+
+11. What are the shippers and the total number of orders shipped by each shipper, including those with no orders?
+
+12. What are the employees who have processed > 5 orders? Sort the result by the number of orders in descending order.
+
+13. What is the total revenue for each product within each order, including the product name and ordered by order ID and total revenue in descending order?
+
+14. What are the customers, employees, and the total number of orders placed by each customer?
+
+15. What are the products with an average price higher than the overall average product price? 
+    Round the price to 2 decimal places and sort the result by price in descending order.
+
