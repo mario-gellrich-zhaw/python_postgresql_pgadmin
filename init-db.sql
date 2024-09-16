dc75e45b3a61bc0dd63553cc0868059e8a4d1759
mario-gellrich-zhaw <mario.gellrich@zhaw.ch>
2024-09-16 14:54:06 +0200

    ...

--- init-db.sql
+++ init-db.sql
@@ -1,5 +1,5 @@
 -- init-db.sql
 CREATE ROLE postgres WITH LOGIN PASSWORD 'geheim' SUPERUSER;
 
--- Create the northwind database
+-- Create a new (empty) database named 'northwind'
 CREATE DATABASE northwind;
